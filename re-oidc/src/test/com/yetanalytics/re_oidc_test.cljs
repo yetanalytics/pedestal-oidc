616a4f1be25a8a49b2e45a2a16f2565d45a5baec
Milt Reder <milt@yetanalytics.com>
2022-02-01 16:08:54 -0500

    remove re-oidc

--- re-oidc/src/test/com/yetanalytics/re_oidc_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns com.yetanalytics.re-oidc-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [com.yetanalytics.re-oidc :as re-oidc]))
-
-(deftest multiply-test
-  (is true))
